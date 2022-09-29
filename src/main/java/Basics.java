b096494e9c03697462221262bfeb44a49fb3a79c
xiaoxinamek <xiaoxi.tang@mail.utoronto.ca>
2022-09-29 04:20:32 -0400

    unsolved problem in oddSum, but I can it worth to find out why it has error, this is the copy of an error version

--- src/main/java/Basics.java
+++ src/main/java/Basics.java
@@ -43,7 +43,7 @@ public class Basics {
         /* TODO (Task 1): Write a line of code below that prints the string
          *                Hello World!
          */
-
+        System.out.println("Hello World!");
 
 
         /* 4. In Python, we could declare variables using a simple assignment
@@ -62,6 +62,7 @@ public class Basics {
          * TODO (Task 2): Create a variable named my_variable and assign it the
          *                value 100.
          */
+        int my_variable = 100;
 
 
 
@@ -99,6 +100,9 @@ public class Basics {
          * Current count: 1
          * Current count: 0
          */
+        for (int i = 10; i >= 0; i--) {
+            System.out.println("Current count: " + i );
+        }
 
 
     }
@@ -143,6 +147,10 @@ public class Basics {
         StringBuilder ret = new StringBuilder();
 
         // Fill in the rest of the body here
+        String[] word_list = to_split.split(" ");
+        for (String x : word_list) {
+            ret.append(x.charAt(0));
+        }
 
         return ret.toString();
     }
@@ -170,7 +178,12 @@ public class Basics {
          *                You can index into arrays as we do in Python
          *                (e.g. arr[i] gives you the item at index i).
          */
+        int key = 0;
+        for (int number :  arr) {
+            key ++;
+            current_sum += key % 2 * number;
 
+        }
         return current_sum;
     }
 
